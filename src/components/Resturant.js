b6e4ca78c6d810f13ffedbd2b77c9127e58959a2
AbdulAhad <pasha49557@gmail.com>
2022-05-26 00:58:02 +0500

    Create and design Resturant Menu App

---
+++ src/components/Resturant.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import "./Style.css";
+import Menu from "./MenuApi.js";
+import MenuCard from "./MenuCards";
+import Navbar from "./Navbar";
+
+const uniqueList = [
+  ...new Set(
+    Menu.map((curElem) => {
+      return curElem.category;
+    })
+  ),
+  "All",
+];
+
+console.log(uniqueList);
+
+const Resturant = () => {
+  const [menuData, setMenuData] = useState(Menu);
+  const [menuList, setMenuList] = useState(uniqueList);
+
+  const filterItem = (category) => {
+    if (category === "All") {
+      setMenuData(Menu);
+      return;
+    }
+
+    const updatedList = Menu.filter((curElem) => {
+      return curElem.category === category;
+    });
+
+    setMenuData(updatedList);
+  };
+
+  return (
+    <>
+      <Navbar filterItem={filterItem} menuList={menuList} />
+      <div className="MenuCard_main">
+      <MenuCard menuData={menuData} />
+      </div>
+      
+    </>
+  );
+};
+
+export default Resturant;
